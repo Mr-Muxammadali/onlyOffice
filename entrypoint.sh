27619848e025b82027cc7e5943df2be3b882093b
Muxammadali <117062708+Mr-Muxammadali@users.noreply.github.com>
2026-04-18 18:48:20 +0500

    Update entrypoint.sh for migrations and execution
    Uncomment migration command and fix entrypoint execution.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,12 @@
 set -e
 
 echo "Running migrations..."
-#php artisan migrate --force
+php artisan migrate --force
 
 echo "Clearing caches..."
 php artisan optimize:clear
 
 echo "Starting application..."
-# Webdevops imeyjining asl entrypointini chaqiramiz
-exec entrypoint.sh
+# Webdevops imeyjining asl entrypoint fayli ildiz papkada joylashgan
+# Shuning uchun oldiga "/" qo'yish shart
+exec /entrypoint.sh
