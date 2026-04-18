6d882b611c8ae606d369ecfef7de82c271ba354e
Muxammadali <117062708+Mr-Muxammadali@users.noreply.github.com>
2026-04-18 17:52:37 +0500

    Comment out migration command in entrypoint.sh
    Comment out the migration command in entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+echo "Running migrations..."
+#php artisan migrate --force
+
+echo "Clearing caches..."
+php artisan optimize:clear
+
+echo "Starting application..."
+# Webdevops imeyjining asl entrypointini chaqiramiz
+exec /entrypoint.sh
